52e9354ba7dd1b83832a38b35ac55db88c68a3ef
eduxyun <yunze.du@ericsson.com>
2020-10-23 14:38:46 +0800

    support graceful shutdown in spring boot 2.3 version

--- recordservice-common/src/main/java/com/dyz/recordservice/common/config/GracefulShutdownConfiguration.java
+++ recordservice-common/src/main/java/com/dyz/recordservice/common/config/GracefulShutdownConfiguration.java
@@ -13,30 +13,10 @@ import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Configuration
-public class GracefulShutdownConfiguration  implements TomcatConnectorCustomizer, ApplicationListener<ContextClosedEvent> {
+public class GracefulShutdownConfiguration implements ApplicationListener<ContextClosedEvent> {
 
-    private volatile Connector connector;
-
-    @Override
-    public void customize(Connector connector) {
-        this.connector = connector;
-    }
     @Override
     public void onApplicationEvent(ContextClosedEvent contextClosedEvent) {
-        log.info("shutdown application!");
-        this.connector.pause();
-        Executor executor = this.connector.getProtocolHandler().getExecutor();
-        if (executor instanceof ThreadPoolExecutor) {
-            try {
-                ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) executor;
-                threadPoolExecutor.shutdown();
-                int waitTime = 30;
-                if (!threadPoolExecutor.awaitTermination(waitTime, TimeUnit.SECONDS)) {
-                    log.warn("Tomcat thread pool did not shut down gracefully within " + waitTime + " seconds. Proceeding with forceful shutdown");
-                }
-            } catch (InterruptedException ex) {
-                Thread.currentThread().interrupt();
-            }
-        }
+
     }
 }
